ec9793c0ac274abacdfeff4dfa0801c5f36c0520
Júlia Emmylly de Lima Monteiro <20211170150007@ifrn.local>
2023-12-29 09:18:25 -0300

    Quase finalizado!

--- CobrinhaJogo/Assets/Scripts/GameManage.cs
+++ CobrinhaJogo/Assets/Scripts/GameManage.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManage : MonoBehaviour
 {
@@ -30,5 +32,21 @@ public class GameManage : MonoBehaviour
       }
       
       Time.timeScale = 0;
+   }
+
+   public void Start()
+   {
+      Debug.Log(SceneManager.GetActiveScene().name);
+      string nomedacena = SceneManager.GetActiveScene().name;
+      if (nomedacena == "jogo")
+      {
+         Debug.Log("apagar cena");
+         gameOverPanel.SetActive(false);
+      }
+   }
+
+   public void Inicio()
+   {
+      SceneManager.LoadScene(1);
    }
 }
